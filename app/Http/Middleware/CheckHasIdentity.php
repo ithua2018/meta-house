9341e7dc87f572dbb49591f5666e5def1ae5c278
lijinshan <1919437986@qq.com>
2022-08-31 22:29:17 +0800

    [登录注册优化]新用户注册登录时，设置密码此时不需要校验用户身份.

--- app/Http/Middleware/CheckHasIdentity.php
+++ app/Http/Middleware/CheckHasIdentity.php
@@ -3,7 +3,6 @@
 namespace App\Http\Middleware;
 
 use App\Constants\ResponseCode;
-use App\Constants\UserConstants;
 use App\Models\UsersInformation;
 use Closure;
 use Illuminate\Support\Facades\Auth;
@@ -19,26 +18,30 @@ class CheckHasIdentity
      */
     public function handle($request, Closure $next)
     {
-//           $user = Auth::guard('api')->user();
-//           // console_debug($user);
-//            if(empty($user['current_role'])) {
-//                return response()->json([
-//                    'success' => false,
-//                    'data' =>[],
-//                    'errorCode' => ResponseCode::AUTH_NOT_SELECT_ROLE[0],
-//                    'errorMessage' => ResponseCode::AUTH_NOT_SELECT_ROLE[1]
-//                ],401);
-//            } else {
-//                $userInfo = UsersInformation::query()->where(['user_id' => $user['id'], 'role'=>$user['current_role']])->get()->toArray();
-//               if(empty($userInfo)) {
-//                   return response()->json([
-//                       'success' => false,
-//                       'data' =>[],
-//                       'errorCode' => ResponseCode::AUTH_NOT_SELECT_ROLE[0],
-//                       'errorMessage' => ResponseCode::AUTH_NOT_SELECT_ROLE[1]
-//                   ],401);
-//               }
-//            }
+        if ($request->path() == 'm/user/regPwd'){
+            //首次登录设置密码时，先不校验用户身份,客户端可以通过post m/me 返回的userinfo=>role来判断.
+            return $next($request);
+        }
+           $user = Auth::guard('api')->user();
+           // console_debug($user);
+            if(empty($user['current_role'])) {
+                return response()->json([
+                    'success' => false,
+                    'data' =>[],
+                    'errorCode' => ResponseCode::AUTH_NOT_SELECT_ROLE[0],
+                    'errorMessage' => ResponseCode::AUTH_NOT_SELECT_ROLE[1]
+                ],401);
+            } else {
+                $userInfo = UsersInformation::query()->where(['user_id' => $user['id'], 'role'=>$user['current_role']])->get()->toArray();
+               if(empty($userInfo)) {
+                   return response()->json([
+                       'success' => false,
+                       'data' =>[],
+                       'errorCode' => ResponseCode::AUTH_NOT_SELECT_ROLE[0],
+                       'errorMessage' => ResponseCode::AUTH_NOT_SELECT_ROLE[1]
+                   ],401);
+               }
+            }
         return $next($request);
     }
 }
